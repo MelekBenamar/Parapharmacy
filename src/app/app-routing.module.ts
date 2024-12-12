0adcd9726865544ac58435ead844a04ee5669ecf
Melek Ben Amar <benamar.melek@gmail.com>
2024-12-12 01:18:54 +0100

    some updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProductGridComponent } from './fragments/product-grid/product-grid.comp
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //{ path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'product-list', component: ProductListComponent},
   { path: 'product-grid', component: ProductGridComponent},
